6af9f6beaffec8e543d2b41a8a9f2a3605705e7c
paltisstudios <99456001+paltisstudios@users.noreply.github.com>
2022-02-15 14:30:10 -0300

    Add files via upload

---
+++ sweet alert.js
@@ -0,0 +1,29 @@
+var swal1 = () => swal("Don't do it!", "Your are amaizing just the way you are and theres no need to change", "error");
+
+var swal2 = () => swal("You woke up!", "Bb despiertate y no ande llorando por un bobo que no te quiere como te juro que, te juro que te amo ", "success");
+
+var swal2a = () => swal("I beg you to be carfull please!", "Dime que si, dime que no. Pero no juegues con mi corazón. Lo quieres tu y lo quiero yo", "warning");
+
+var swal3 = () => swal("Attention here!", "Mi corazon esta colgando en tus manos, asi que no lo dejes caer", "warning");
+
+var swal4 = () => swal("Just for your record!", "Andas en mi cabeza nena a todas horas", "info");
+
+var swal5 = () => swal("OMG","Uste por la galaxia me tiene volando, contigo siento que las nubes estoy acariciando, y es que de ti me voy enamorando", "warning")
+
+var swal6 = () => swal("KESI KESIII!","What would you do if I got down on my knees?, What if I flipped our whole world upsidе down, now? You know that we fit together, you'rе my queen","success")
+
+var swal7 = () => swal("Of course you are!","Si es amor, abrázame con ganas.Si no lo es, tal vez será mañana.Estando juntos mi mundo se llena de luz.Lo mejor de mi vida eres tú","success") 
+
+var swal8 = () => swal("Not easy to choose!","La verdad es que no se puede poner alguna frase o verso, ya que todos son los que destacaría","success") 
+
+var swal9 = () => swal("Falling again in love?","Can I fall in love again? Maybe you're the one I needed, To take it all away,this pain, Can you fall in love with me?,And tell me that you'll care for me","warning") 
+
+var swal10 = () => swal("WHAAAT HIT ME?","I'm wishing I could show you all these feelings that I felt, You've got a hold of me, I'm diving in head first, Hoping I could love you so recklessly, It hit me like a tidal wave I'm falling off, You got me hypnotized, mesmerized","info") 
+
+var swal11 = () => swal("My heart beat faster, HELP!","Aceleras to' mis latidos, Y e' que me gusta todo de ti, De to'a tus parte', ¿cuál decido?, Y e' que me gusta todo de ti","warning") 
+
+var swal12 = () => swal("Are you in to enjoy life togehter?","What you say?, You and me, Forget about the past and throw it in the trash,  What you say?, You and me,  Live the life we never had like we're never going back.","info") 
+
+var swal13 = () => swal("Happier Than ever?","You were my everything, and now what you did is to make me feel sad","error") 
+
+var swal14 = () => swal("I was never an option for you?","Pude haber sido yo la que tiene tu corazón guardado, Pero alguien sin piedad me lo robó, Cuando por fin pensé haberlo atrapado, Fue que se escapo","error") 
